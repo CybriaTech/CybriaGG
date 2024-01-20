bbe309cc27515d903d8f89b0201a7f3811170670
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-01-19 23:13:24 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const targetURL = 'https://educationbluesky.com'; // Link where the NowGG roblox gets proxied as CybriaGG <3
+
+const proxy = httpProxy.createProxyServer({
+  target: targetURL,
+  changeOrigin: true,
+});
+
+app.use('/', (req, res) => {
+  console.log(`Incoming Requests: ${req.url}`);
+
+  proxy.web(req, res);
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', 'default-src "self"; style-src "self"');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<html><head>');
+
+  res.write('<title>CybriaGG | Play Roblox Online</title>'); // This is still WIP, ill fix it eventually
+
+  res.write('<link rel="icon" href="/public/cygg-invert-32x32.png">'); // This is still WIP, ill fix it eventually
+
+  res.write('</head><body>');
+
+  next();
+});
+
+
+const server = http.createServer(app);
+
+const PORT = 80;
+server.listen(PORT, () => {
+  console.log(`CybriaGG Has Been Successfully Run! On Port ${PORT}`);
+});
