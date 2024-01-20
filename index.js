62ad9db8f50e9d50650e8aacad4d701d9bbf2c14
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-01-20 12:40:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,65 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const express = require('express');
 const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const app = express();
+if (cluster.isMaster) {
+  console.log(`Slave ${process.pid} is running`);
 
-const nggUrl = 'https://educationbluesky.com'; // Link where the NowGG roblox gets proxied as CybriaGG <3
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-const proxy = httpProxy.createProxyServer({
-  target: nggUrl,
-  changeOrigin: true,
-});
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
 
-app.use('/', (req, res) => {
-  console.log(`Incoming Requests: ${req.url}`);
+  const targetURL = 'https://educationbluesky.com'; // The Link Where Now.GG Roblox Gets Proxied
 
-  proxy.web(req, res);
-});
+  const proxy = httpProxy.createProxyServer({
+    target: targetURL,
+    changeOrigin: true,
+  });
 
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', 'default-src "self"; style-src "self"');
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'DENY');
-  res.setHeader('X-XSS-Protection', '1; mode=block');
-  res.setHeader('Content-Type', 'text/html');
+  app.use('/', (req, res) => {
+    console.log(`Incoming Requests: ${req.url}`);
+    proxy.web(req, res);
+  });
 
-  res.write('<html><head>');
+  app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', 'default-src "self"; style-src "self"');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('Content-Type', 'text/html');
 
-  res.write('<title>CybriaGG | Play Roblox Online</title>'); // This is still WIP, ill fix it eventually
+    res.write('<html><head>');
 
-  res.write('<link rel="icon" href="/public/cygg-invert-32x32.png">'); // This is still WIP, ill fix it eventually
+    res.write('<title>CybriaGG | Play Roblox Online</title>'); // Dosent work :(( i fix it soon
 
-  res.write('</head><body>');
+    res.write('<link rel="icon" href="/public/cygg-invert-32x32.png">'); // Dosent work :(( i fix it soon
 
-  next();
-});
+    res.write('</head><body>');
 
+    next();
+  });
 
-const server = http.createServer(app);
+  const server = http.createServer(app);
 
-const PORT = 80;
-server.listen(PORT, () => {
-  console.log(`CybriaGG Has Been Successfully Run! On Port ${PORT}`);
-});
+  const PORT = 80;
+  server.listen(PORT, () => {
+    console.log(`CybriaGG Slave ${process.pid} has been successfully run! On Port ${PORT}`);
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error(`Worker ${process.pid} detected unhandled rejections:`, promise, 'reason:', reason);
+  });
+
+  process.on('uncaughtException', (err) => {
+    console.error(`Worker ${process.pid} uncaught exception:`, err);
+  });
+}
