69157ecbad36251df1d1c36a420d16638c31b514
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-03-19 17:13:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://websitesball.com';
+const nggUrl = 'https://mathsspot.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
